c1cad70d1747faac99a406dda3b3ab93b7d6b11e
281677160 <66651997+281677160@users.noreply.github.com>
2021-02-20 14:02:40 +0800

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,5 +1,5 @@
 Diy_Core() {
-	source "${GITHUB_WORKSPACE}/build/${{matrix.target}}/settings.ini"
+	source "${UPDATE}/settings.ini"
 	echo "Author=${Author}"
 	echo "Default_Device=${Default_Device}"
 	echo "Updete_firmware=${Updete_firmware}"
