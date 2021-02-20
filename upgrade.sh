ff18541a4d41e5851d5c42823f6fa20167e57b14
281677160 <66651997+281677160@users.noreply.github.com>
2021-02-21 02:03:27 +0800

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -5,8 +5,10 @@ GET_TARGET_INFO() {
         SUBTARGET="$(awk -F '[="]+' '/TARGET_SUBTARGET/{print $2}' .config)"
         if [[ "$DEVICE" == "x86" ]]; then
 		TARGET_PROFILE="x86-${SUBTARGET}"
+		Firmware_sfx=".img.gz"
 	elif [[ "$DEVICE" != "x86" ]]; then
 		TARGET_PROFILE="$(egrep -o "CONFIG_TARGET.*DEVICE.*=y" .config | sed -r 's/.*DEVICE_(.*)=y/\1/')"
+		Firmware_sfx="${Extension}"
 	fi
 	Github_Repo="$(grep "https://github.com/[a-zA-Z0-9]" ${GITHUB_WORKSPACE}/.git/config | cut -c8-100)"
 	AutoUpdate_Version="$(awk 'NR==6' package/base-files/files/bin/AutoUpdate.sh | awk -F '[="]+' '/Version/{print $2}')"
@@ -31,7 +33,7 @@ Diy_Part2() {
 	echo "	源码作者: ${ZUOZHE}"
 	echo "	机子型号: ${TARGET_PROFILE}"
 	echo "	固件名字: ${Updete_firmware}"
-	echo "	固件后缀: ${Extension}"
+	echo "	固件后缀: ${Firmware_sfx}"
 	echo "	仓库链接: ${Github_Repo}"
 	echo "	固件作者: ${Author}"
 	echo "	固件版本: Firmware-${Openwrt_Version}"
@@ -44,7 +46,7 @@ Diy_Part2() {
 Diy_Part3() {
 	GET_TARGET_INFO
 	Default_Firmware="${Updete_firmware}"
-	AutoBuild_Firmware="openwrt-${Source}-${TARGET_PROFILE}-Firmware-${Openwrt_Version}${Extension}"
+	AutoBuild_Firmware="openwrt-${Source}-${TARGET_PROFILE}-Firmware-${Openwrt_Version}${Firmware_sfx}"
 	AutoBuild_Detail="openwrt-${Source}-${TARGET_PROFILE}-Firmware-${Openwrt_Version}.detail"
 	Mkdir bin/Firmware
 	echo "Firmware: ${AutoBuild_Firmware}"
