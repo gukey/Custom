51ef74dcb463a7ed866732a98dd5d08984b7c979
281677160 <66651997+281677160@users.noreply.github.com>
2021-02-20 17:27:38 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -2,11 +2,11 @@
 
 Diy_all() {
 echo "all"
-if [ ${{ marget }} == "Lede_x86_64" ]; then
+if [ ${marget} == "Lede_x86_64" ]; then
 echo "11111111111111"
-elif [ ${{ marget }} == "Lienol_x86_64" ]; then
+elif [ ${marget} == "Lienol_x86_64" ]; then
 echo "2222222222222221"
-elif [ ${{ marget }} == "Project_x86_64" ]; then
+elif [ ${marget} == "Project_x86_64" ]; then
 echo "333333333333333333"
 fi
 }
