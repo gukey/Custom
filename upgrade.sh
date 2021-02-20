bfaaf8c11197e4d0d3443506825803935ffe39d8
281677160 <66651997+281677160@users.noreply.github.com>
2021-02-20 14:31:48 +0800

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,5 +1,5 @@
 Diy_Core() {
-	source "${marget}/settings.ini"
+	source "${GITHUB_WORKSPACE}/build/${marget}/settings.ini"
 	echo "Author=${Author}"
 	echo "Default_Device=${Default_Device}"
 	echo "Updete_firmware=${Updete_firmware}"
