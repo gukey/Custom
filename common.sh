191771b917b1e6a1fc7e71e396c47390673f47d0
281677160 <66651997+281677160@users.noreply.github.com>
2021-02-22 01:37:23 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -163,7 +163,7 @@ echo "编译源码: ${Source}"
 echo "源码链接: ${REPO_URL}"
 echo "源码分支: ${REPO_BRANCH}"
 echo "源码作者: ${ZUOZHE}"
-echo "机子型号: ${TARGET_PRO}"
+echo "机子型号: ${NAME1}"
 echo "固件作者: ${Author}"
 echo "仓库链接: ${GITHUB_RELEASE}"
 if [[ ${UPLOAD_BIN_DIR} == "false" ]]; then
