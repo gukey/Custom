4204662853bcb7fc2a3ec023dcb72d026b799f84
281677160 <66651997+281677160@users.noreply.github.com>
2021-02-21 13:56:57 +0800

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -2,13 +2,7 @@ GET_TARGET_INFO() {
 	[ -f ${GITHUB_WORKSPACE}/Openwrt.info ] && . ${GITHUB_WORKSPACE}/Openwrt.info
 	Openwrt_Version="${Compile_Date}"
 	Author="${Author}"
-	DEVICE=$(awk -F '[="]+' '/TARGET_BOARD/{print $2}' .config)
-        SUBTARGET="$(awk -F '[="]+' '/TARGET_SUBTARGET/{print $2}' .config)"
-        if [[ "$DEVICE" == "x86" ]]; then
-		TARGET_PROFILE="x86-${SUBTARGET}"
-	elif [[ "$DEVICE" != "x86" ]]; then
-		TARGET_PROFILE="$(egrep -o "CONFIG_TARGET.*DEVICE.*=y" .config | sed -r 's/.*DEVICE_(.*)=y/\1/')"
-	fi
+	TARGET_PROFILE="${NAME1}"
         if [[ "$DEVICE" == "x86" ]]; then
 		Firmware_sfx=".img.gz"
 	elif [[ "$TARGET_PROFILE" == "phicomm-k3" ]]; then
