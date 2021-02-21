47f90a61ed403f94a77dc6ca45103524c886143c
281677160 <66651997+281677160@users.noreply.github.com>
2021-02-21 14:02:54 +0800

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -73,7 +73,7 @@ Diy_Part2() {
 	fi
 	if [[ ${REGULAR_UPDATE} == "true" ]]; then
 		echo "定时更新: 开启"
-		echo "把定时自动更新编译进固件已开启"
+		echo "把定时自动更新插件编译进固件已开启"
 		echo "请把“REPO_TOKEN”密匙设置好,没设置好密匙不能发布云端地址"
 		echo "请注意核对固件名字和后缀,避免编译错误"
 	fi
