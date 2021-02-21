a389c5caadca668a01c7af725751c9c1b406749d
281677160 <66651997+281677160@users.noreply.github.com>
2021-02-21 15:55:39 +0800

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -78,7 +78,7 @@ Diy_Part2() {
 		echo "微信通知: 开启"
 	fi
 	if [[ ${REGULAR_UPDATE} == "true" ]]; then
-		echo "定时更新: 开启"
+		echo "编译定时更新插件: 开启"
 		echo "把定时自动更新插件编译进固件已开启"
 		echo "请把“REPO_TOKEN”密匙设置好,没设置好密匙不能发布云端地址"
 		echo "请注意核对固件名字和后缀,避免编译错误"
