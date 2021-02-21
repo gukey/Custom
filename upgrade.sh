e8826529b39e4d9ccd7cb35cc1b21c1066e7bba6
281677160 <66651997+281677160@users.noreply.github.com>
2021-02-21 11:45:47 +0800

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -39,41 +39,41 @@ Diy_Part2() {
 	echo "自动更新固件版本: Firmware-${Openwrt_Version}"
 	echo "固件作者: ${Author}"
 	echo "仓库链接: ${Github_Repo}"
-if [[ ${UPLOAD_BIN_DIR} == "false" ]]; then
-	echo "上传BIN文件夹(固件+IPK): 关闭"
-elif [[ ${UPLOAD_BIN_DIR} == "true" ]]; then
-	echo "上传BIN文件夹(固件+IPK): 开启"
-fi
-if [[ ${UPLOAD_CONFIG} == "false" ]]; then
-	echo "上传[.config]配置文件: 关闭"
-elif [[ ${UPLOAD_CONFIG} == "true" ]]; then
-	echo "上传[.config]配置文件: 开启"
-fi
-if [[ ${UPLOAD_FIRMWARE} == "false" ]]; then
-	echo "上传固件在github空间: 关闭"
-elif [[ ${UPLOAD_FIRMWARE} == "true" ]]; then
-	echo "上传固件在github空间: 开启"
-fi
-if [[ ${UPLOAD_COWTRANSFER} == "false" ]]; then
-	echo "上传固件到到【奶牛快传】和【WETRANSFER】: 关闭"
-elif [[ ${UPLOAD_COWTRANSFER} == "true" ]]; then
-	echo "上传固件到到【奶牛快传】和【WETRANSFER】: 开启"
-fi
-if [[ ${UPLOAD_RELEASE} == "false" ]]; then
-	echo "发布固件: 关闭"
-elif [[ ${UPLOAD_RELEASE} == "true" ]]; then
-	echo "发布固件: 开启"
-fi
-if [[ ${SERVERCHAN_SCKEY} == "false" ]]; then
-	echo "微信通知: 关闭"
-elif [[ ${SERVERCHAN_SCKEY} == "true" ]]; then
-	echo "微信通知: 开启"
-fi
-if [[ ${REGULAR_UPDATE} == "true" ]]; then
-	echo "把定时自动更新编译进固件已开启"
-	echo "请把“REPO_TOKEN”密匙设置好,没设置好密匙不能发布云端地址"
-	echo "请注意核对固件名字和后缀,避免编译错误"
-fi
+	if [[ ${UPLOAD_BIN_DIR} == "false" ]]; then
+		echo "上传BIN文件夹(固件+IPK): 关闭"
+	elif [[ ${UPLOAD_BIN_DIR} == "true" ]]; then
+		echo "上传BIN文件夹(固件+IPK): 开启"
+	fi
+	if [[ ${UPLOAD_CONFIG} == "false" ]]; then
+		echo "上传[.config]配置文件: 关闭"
+	elif [[ ${UPLOAD_CONFIG} == "true" ]]; then
+		echo "上传[.config]配置文件: 开启"
+	fi
+	if [[ ${UPLOAD_FIRMWARE} == "false" ]]; then
+		echo "上传固件在github空间: 关闭"
+	elif [[ ${UPLOAD_FIRMWARE} == "true" ]]; then
+		echo "上传固件在github空间: 开启"
+	fi
+	if [[ ${UPLOAD_COWTRANSFER} == "false" ]]; then
+		echo "上传固件到到【奶牛快传】和【WETRANSFER】: 关闭"
+	elif [[ ${UPLOAD_COWTRANSFER} == "true" ]]; then
+		echo "上传固件到到【奶牛快传】和【WETRANSFER】: 开启"
+	fi
+	if [[ ${UPLOAD_RELEASE} == "false" ]]; then
+		echo "发布固件: 关闭"
+	elif [[ ${UPLOAD_RELEASE} == "true" ]]; then
+		echo "发布固件: 开启"
+	fi
+	if [[ ${SERVERCHAN_SCKEY} == "false" ]]; then
+		echo "微信通知: 关闭"
+	elif [[ ${SERVERCHAN_SCKEY} == "true" ]]; then
+		echo "微信通知: 开启"
+	fi
+	if [[ ${REGULAR_UPDATE} == "true" ]]; then
+		echo "把定时自动更新编译进固件已开启"
+		echo "请把“REPO_TOKEN”密匙设置好,没设置好密匙不能发布云端地址"
+		echo "请注意核对固件名字和后缀,避免编译错误"
+	fi
 }
 	echo "Firmware-${Openwrt_Version}" > package/base-files/files/etc/openwrt_info
 	echo "${Github_Repo}" >> package/base-files/files/etc/openwrt_info
